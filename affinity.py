433b398bfc920ae03e7103e47e444e3c05c2a376
jrugis <jrugis@gmail.com>
2019-03-15 17:54:21 +1300

    more code

--- affinity.py
+++ affinity.py
@@ -4,15 +4,16 @@
 
 import psutil
 
-print(psutil.cpu_count())
+print(psutil.cpu_count(logical=False))
 
 p = psutil.Process()
-print(p.cpu_affinity())
+print(dir(p))
+print(p.cpu_affinity())  # not defined on mac
 
 p.cpu_affinity([0, 1])
-print(p.cpu_affinity())
+print(p.cpu_affinity())  # not defined on mac
 
 p.cpu_affinity([])
-print(p.cpu_affinity())
+print(p.cpu_affinity())  # not defined on mac
 
 
