0e5a70f7f216b28322f853610570ebfafea28d81
jrugis <jrugis@gmail.com>
2019-03-17 17:31:57 +1300

    numpy arrays

--- test00.py
+++ test00.py
@@ -2,25 +2,41 @@
 # run time test
 #
 #  mac:         3.4
-#  nectar:      22
-#  nesi build:
-#  nesi run:
-
+#  nectar:      22 (4.5)
+#  nesi build:  4.3
+#  nesi run n4: 4.3
+#  nesi run n8: 4.3 
+    
 import time
 import numpy as np
 
-# execution timer
+# for loop
 start = time.time()
-
-incr = np.double(0.1)
+incr = 0.1
 x = incr
 for i in range(1000000):
     k = np.log(x)
     l = np.sin(x)
     m = np.cos(x)
     x += incr
-    
-# execution timer
+end = time.time()
+print(end - start)
+
+# numpy array
+start = time.time()
+x = np.arange(start=0.1, stop=10000.0, step=0.1, dtype="double")
+k = np.log(x)
+l = np.sin(x)
+m = np.cos(x)
+end = time.time()
+print(end - start)
+
+# numpy iterate
+start = time.time()
+for t in x:
+    k = np.log(t)
+    l = np.sin(t)
+    m = np.cos(t)
 end = time.time()
 print(end - start)
 
---
+++ test01.py
@@ -0,0 +1,15 @@
+#
+# run time test
+#
+    
+import time
+import numpy as np
+
+# execution timer
+start = time.time()
+
+
+# execution timer
+end = time.time()
+print(end - start)
+
